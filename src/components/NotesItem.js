d547914215c475a49715f4a00aebc6f63bf6dcf4
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-06-16 16:05:41 +0700

    (refactor): change date formatting from child to parent notesitem

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -4,7 +4,8 @@ import NotesItemBody from "./NotesItemBody";
 const NotesItem = ({ title, body, createdAt, dateFormatter }) => {
 	return (
 		<div className="notes-item">
-			<NotesItemBody />
+			<NotesItemBody title={title} body={body} formattedCreatedAt={dateFormatter(createdAt)} />
+			
 		</div>
 	)
 }
--- src/components/NotesItemBody.js
+++ src/components/NotesItemBody.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const NotesItemBody = ({ title, body, createdAt, dateFormatter }) => {
+const NotesItemBody = ({ title, body, formattedCreatedAt }) => {
 	return (
 		<div className="notes-item">
 			<h3>{title}</h3>
 			<p className="notes-item__body">{body}</p>
-			<p className="notes-item__createdAt">{dateFormatter(createdAt)}</p>
+			<p className="notes-item__createdAt">{formattedCreatedAt}</p>
 		</div>
 	)
 }
